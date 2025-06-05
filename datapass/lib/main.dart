671b60ca31e9d5e097ac82b97231c1dd8f1e4c35
Rohitkumarsingh12 <153908585+Rohitkumarsingh12@users.noreply.github.com>
2025-06-05 09:44:19 +0530

    rohit
    crud

---
+++ datapass/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Center(
+       child: Column(
+         children: [
+           Padding(padding: EdgeInsets.only(),
+             child: CircleAvatar(
+               radius: 80,
+               backgroundImage: ,
+             ),
+           )
+         ],
+       ),
+     ),
+    );
+  }
+}
